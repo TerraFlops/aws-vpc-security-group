205f27808449ba07a86a5fbdc3119e4236753bfa
Damian Sloane <damian@capacitorlabs.com>
2020-07-31 16:41:28 +1000

    Work in progress

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "aws_security_group" "security_groups" {
   # Convert name to camel case
   name = join("", [for element in split("_", replace(lower(each.key), "-", "_")): title(element)])
   # Optionally append the VPC description to the end of the description if multiple VPCs are sharing same account
-  description = var.append_vpc_description == true ? "${each.value} (${data.aws_vpc.vpc.tags["Description"]})" : each.value.description
+  description = var.append_vpc_description == true ? "${each.value} (${data.aws_vpc.vpc.tags["Description"]})" : each.value
   vpc_id = var.vpc_id
   tags = {
     Name = join("", [for element in split("_", replace(lower(each.key), "-", "_")): title(element)])
-    Description = var.append_vpc_description == true ? "${each.value} (${data.aws_vpc.vpc.tags["Description"]})" : each.value.description
+    Description = var.append_vpc_description == true ? "${each.value} (${data.aws_vpc.vpc.tags["Description"]})" : each.value
   }
 }
